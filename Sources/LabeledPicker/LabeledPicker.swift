f12d44ba1b02c89204bda39a8902828286ad7a97
leoMehlig <leo@leomehlig.com>
2021-11-19 17:54:18 +0100

    Update

--- Sources/LabeledPicker/LabeledPicker.swift
+++ Sources/LabeledPicker/LabeledPicker.swift
@@ -2,11 +2,15 @@ import SwiftUI
 import UIKit
 
 public enum Column {
-    case _value(_ selected: Binding<Int>, size: Int, content: (Int) -> AnyView)
+    case _value(_ selected: Binding<Int>, size: Int, label: (() -> AnyView)? = nil,  content: (Int) -> AnyView)
     case _label(content: () -> AnyView)
     
     public static func value<Content: View>(_ selected: Binding<Int>, size: Int, content: @escaping (Int) -> Content) -> Column {
         return ._value(selected, size: size, content: { AnyView(content($0)) })
+    }
+    
+    public static func value<Content: View, Label: View>(_ selected: Binding<Int>, size: Int, @ViewBuilder label: @escaping () -> Label, @ViewBuilder content: @escaping (Int) -> Content) -> Column {
+        return ._value(selected, size: size, label: { AnyView(label()) }, content: { AnyView(content($0)) })
     }
     
     public static func label<Content: View>(content: @escaping () -> Content) -> Column {
@@ -15,7 +19,7 @@ public enum Column {
     
     var internalColumn: CustomPickerView.Column {
         switch self {
-        case ._value(_, let size, _):
+        case ._value(_, let size, _, _):
             return .value(size: size)
         case ._label(_):
             return .label
@@ -24,10 +28,19 @@ public enum Column {
     
     var content: (Int) -> AnyView {
         switch self {
-        case ._value(_, _, let content):
+        case ._value(_, _, _, let content):
             return content
         case ._label(let content):
             return { _ in content() }
+        }
+    }
+    
+    var label: (() -> AnyView)? {
+        switch self {
+        case ._value(_, _, let label, _):
+            return label
+        case ._label(let content):
+            return content
         }
     }
 }
@@ -60,6 +73,7 @@ struct LabeledPickerWrapper: UIViewRepresentable {
     func makeUIView(context: Context) -> CustomPickerView {
         let picker = CustomPickerView(columns: self.columns.map({ $0.internalColumn }),
                                       selected: self.selected,
+                                      labels: self.labels,
                                       views: self.views)
         return picker
     }
@@ -68,7 +82,7 @@ struct LabeledPickerWrapper: UIViewRepresentable {
         picker.views = self.views
         picker.columns = self.columns.map({ $0.internalColumn })
         for (index, column) in columns.enumerated() {
-            if case ._value(let selected, _, _) = column {
+            if case ._value(let selected, _, _, _) = column {
                 picker.select(column: index, row: selected.wrappedValue, animated: true)
             }
         }
@@ -81,8 +95,17 @@ struct LabeledPickerWrapper: UIViewRepresentable {
         return hosting
     }
     
+    func labels(column: Int, resusable: UIView?) -> UIView? {
+        guard let label = self.columns.safe(at: column)?.label?() else {
+            return nil
+        }
+        let hosting = resusable as? UIHostingView<AnyView> ?? UIHostingView<AnyView>()
+        hosting.set(value: label)
+        return hosting
+    }
+    
     func selected(column: Int, row: Int) {
-        if case ._value(let selected, _, _)? = self.columns.safe(at: column) {
+        if case ._value(let selected, _, _, _)? = self.columns.safe(at: column) {
             selected.wrappedValue = row
         }
     }
--- Sources/LabeledPicker/UIPicker.swift
+++ Sources/LabeledPicker/UIPicker.swift
@@ -24,6 +24,8 @@ class CustomPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSour
     }
     var views: (Int, Int, UIView?) -> UIView
     
+    var labels: (Int, UIView?) -> UIView?
+    
     var selected: (Int, Int) -> Void
     
     func select(column: Int, row: Int, animated: Bool = false) {
@@ -36,14 +38,22 @@ class CustomPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSour
     var columns: [Column] {
         didSet {
             self.reloadAllComponents()
+//            self.labelViews.keys.forEach {
+//                if self.columns.safe(at: $0)?.isLabel ?? false {
+//                    self.labelViews[$0]?.removeFromSuperview()
+//                    self.labelViews[$0] = nil
+//                }
+//            }
         }
     }
         
     init(columns: [Column],
          selected: @escaping (Int, Int) -> Void,
+         labels: @escaping (Int, UIView?) -> UIView?,
          views: @escaping (Int, Int, UIView?) -> UIView) {
         self.columns = columns
         self.selected = selected
+        self.labels = labels
         self.views = views
         super.init(frame: .zero)
         self.delegate = self
@@ -68,6 +78,11 @@ class CustomPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSour
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let view = self.views(component, row, view)
+        if self.columns.safe(at: component)?.isLabel ?? false {
+             view.alpha = 0
+         } else {
+             view.alpha = 1
+         }
         return view
     }
     
@@ -78,14 +93,33 @@ class CustomPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSour
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    private var labelViews: [Int: UIView] = [:]
+
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        for (index, column) in columns.enumerated() {
-            if column.isLabel,
-               let view = self.view(forRow: 0, forComponent: index)?.searchSuperviews(for: "UIPickerTableView", maxDepth: 5) as? UIScrollView {
-                view.isScrollEnabled = false
+//        for (index, column) in columns.enumerated() {
+//            if column.isLabel,
+//               let view = self.view(forRow: 0, forComponent: index)?.searchSuperviews(for: "UIPickerTableView", maxDepth: 5) as? UIScrollView {
+//                view.isScrollEnabled = false
+//            }
+//        }
+        
+        for index in columns.indices {
+            let rowSize = self.rowSize(forComponent: index)
+            if let view = self.labels(index, self.labelViews[index]), let reference = self.view(forRow: 0, forComponent: index) {
+                if view !== self.labelViews[index] {
+                    self.labelViews[index]?.removeFromSuperview()
+                    self.labelViews[index] = view
+                }
+                if view.superview !== self {
+                    self.addSubview(view)
+                }
+                view.frame = self.convert(reference.bounds, from: reference)
+            } else {
+                self.labelViews[index]?.removeFromSuperview()
+                self.labelViews[index] = nil
             }
         }
     }
