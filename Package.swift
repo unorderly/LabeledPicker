9b87ca6bae2ea167eaf9e10ef55060b7da95d1a4
Leo Mehlig <leo@leomehlig.com>
2025-05-03 13:09:49 +0200

    Format

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(name: "LabeledPicker",
                       platforms: [
-                        .iOS(.v16), .watchOS(.v9), .macCatalyst(.v16), .macOS(.v13)
+                          .iOS(.v16), .watchOS(.v9), .macCatalyst(.v16), .macOS(.v13)
                       ],
                       products: [
                           .library(name: "LabeledPicker",
